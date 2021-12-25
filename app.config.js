b1431efb542588c4b5f8e2d077d27ffe1e0d5997
Andres Torres <andres@altomobile.io>
2021-12-24 23:55:45 -0600

    + libraries added; config file modified; env file added

---
+++ app.config.js
@@ -0,0 +1,38 @@
+import 'dotenv/config'
+
+export default {
+    expo: {
+        name: "TribalHomeTest",
+        slug: "tribal-home-test",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/icon.png",
+        splash: {
+            image: "./assets/splash.png",
+            resizeMode: "contain",
+            backgroundColor: "#ffffff"
+        },
+        updates: {
+            fallbackToCacheTimeout: 0
+        },
+        assetBundlePatterns: [
+            "**/*"
+        ],
+        ios: {
+            supportsTablet: true
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/adaptive-icon.png",
+                backgroundColor: "#FFFFFF"
+            }
+        },
+        web: {
+            favicon: "./assets/favicon.png"
+        },
+        extra: {
+            apiUrl: process.env.API_URL,
+            apiKey: process.env.API_KEY,
+        },
+    }
+}
