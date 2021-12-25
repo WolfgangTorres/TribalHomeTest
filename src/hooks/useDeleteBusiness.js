a9d8e4fc650bb25d7de16ce148a833ea6786647e
Andres Torres <andres@altomobile.io>
2021-12-25 02:38:59 -0600

    + react query hooks

---
+++ src/hooks/useDeleteBusiness.js
@@ -0,0 +1,24 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useAxios } from '../../src/services';
+
+const deleteBusiness = async (businessId) => {
+    const result = await useAxios({
+        method: 'delete',
+        url: `/business/${businessId}`,
+    });
+
+    return result;
+};
+
+export const useDeleteBusiness = () => {
+    const queryClient = useQueryClient();
+
+    return useMutation(
+        (businessId) => deleteBusiness(businessId),
+        {
+            onSuccess: (data, variables, context) => {
+                queryClient.invalidateQueries()
+            },
+        }
+    );
+};
